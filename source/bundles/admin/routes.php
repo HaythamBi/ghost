e134f1598704e878afc43a74083afee8f5f75ed4
Wayne Berry <wayneashleyberry@gmail.com>
2012-11-06 16:36:44 +0200

    move admin stuff into bundle

---
+++ source/bundles/admin/routes.php
@@ -0,0 +1,70 @@
+<?php
+
+# Public Routes
+
+Route::get('admin/login', array('as' => 'login', function ()
+{
+	return 'login';
+}));
+
+# Private Routes
+
+Route::group(array('before' => 'auth'), function()
+{
+	# Dashboard
+
+	Route::get('(:bundle)', array('as' => 'dashboard', 'before' => 'auth', function ()
+	{
+		return 'dashboard';
+	}));
+
+	# Posts
+
+	Route::get('(:bundle)/posts', array('as' => 'blog', function ()
+	{
+		return 'blog';
+	}));
+
+	# New Post
+
+	Route::get('(:bundle)/posts/new', function ()
+	{
+		return 'new post';
+	});
+
+	# Edit Post
+
+	Route::get('(:bundle)/posts/(:num)', function ()
+	{
+		return 'edit post';
+	});
+
+	# Authors
+
+	Route::get('(:bundle)/authors', array('as' => 'authors', function ()
+	{
+		return 'authors';
+	}));
+
+	# Edit Author
+
+	Route::get('(:bundle)/authors/(:num)', function ($id)
+	{
+		return 'author ' . $id;
+	});
+
+	# Analytics
+
+	Route::get('(:bundle)/analytics', array('as' => 'analytics', function ()
+	{
+		return 'analytics';
+	}));
+
+	# Settings
+
+	Route::get('(:bundle)/settings', array('as' => 'dashboard', function ()
+	{
+		return 'settings';
+	}));
+
+});
