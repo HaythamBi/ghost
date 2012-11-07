74092b34a943d9ee05094798f7023801ae7fb04c
Wayne Berry <wayneashleyberry@gmail.com>
2012-11-07 11:52:54 +0200

    tweak nav block

--- source/application/views/dashboard/main.blade.php
+++ source/application/views/dashboard/main.blade.php
@@ -5,8 +5,9 @@
 				<li class="navblock-text"><strong>Welcome back,</strong> {{ Auth::user()->name }}.</li>
 				<li><a class="muted uppercase" href="{{ URL::to_route('New Post') }}"><i>&#9998;</i> New Post</a></li>
 				<li><a class="muted uppercase" href="{{ URL::to_route('Authors') }}"><i>&#128101;</i> Authors</a></li>
+				<li><a class="muted uppercase" href="#"><i>&#59160;</i> Comments</a></li>
 				<li><a class="muted uppercase" href="{{ URL::to_route('Analytics') }}"><i>&#128200;</i> Analytics</a></li>
-				<li><a class="muted uppercase" href="#"><i>&#128269;</i> Search</a></li>
+				<li class="pull-right"><a class="muted uppercase" href="#"><i>&#128269;</i> Search</a></li>
 			</ul>
 		</div>
 	</div>
